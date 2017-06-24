2d6b488b8327f288588a47d90697d84f8a736438
StudentWan <benyuwan@gmail.com>
2017-06-24 14:25:02 +0800

    complete task05 part1

--- task05/task05-2/main.js
+++ task05/task05-2/main.js
@@ -4,11 +4,11 @@ const opts = document.getElementsByClassName('nav-opt');
 for(let opt of opts) {
   console.log(opt);
   opt.addEventListener('mouseover', (e) => {
-    opt.style.background = 'url(https://raw.githubusercontent.com/StudentWan/efe-work-task/master/task02/static/img/arrow.png) 95% 50% no-repeat,#303030';
+    opt.style.background = 'url(https://raw.githubusercontent.com/StudentWan/efe-work-task/master/task02/static/img/arrow.png) 99% 50% no-repeat,#303030';
     opt.children[0].style.display = 'block';
   })
   opt.addEventListener('mouseout', (e) => {
-    opt.style.background = 'url(https://raw.githubusercontent.com/StudentWan/efe-work-task/master/task02/static/img/arrow.png) 95% 50% no-repeat';
+    opt.style.background = 'url(https://raw.githubusercontent.com/StudentWan/efe-work-task/master/task02/static/img/arrow.png) 99% 50% no-repeat';
     opt.children[0].style.display = 'none';
   })
 }
