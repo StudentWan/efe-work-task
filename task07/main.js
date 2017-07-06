9bd5d381953ecb0996114e98dc66581f35a8077f
StudentWan <benyuwan@gmail.com>
2017-07-06 22:21:12 +0800

    add: complete task07

--- task07/main.js
+++ task07/main.js
@@ -1,5 +1,5 @@
 /**
- * @file 用来实现任务06的需求，包括：左边栏滚动、长度实时变化、悬浮表头等
+ * @file 用来实现任务07的需求，包括：左边栏二级项目展示、浮出层、表格项目控制等
  * @author benyuwan(benyuwan@gmail.com)
  */
 
@@ -7,26 +7,42 @@ const table = document.getElementById('main-table');
 const head = document.getElementById('head-table');
 
 let tableContent = table.children[0].innerHTML;
-
 // 根据数据文件table.js动态生成表格
 for (let i = 0, lens = Object.keys(content).length; i < lens; i++) {
-    let eachLine = '<tr><td>' + content[i].name + 
-                   '</td><td>' + content[i].content + 
-                   '</td><td>' + content[i].value + 
-                   '</td><td><button class="edit">Edit</button> <button class="delete">Delete</button></td></tr>';
+    let eachLine = '<tr><td>' + content[i].name +
+        '</td><td>' + content[i].content +
+        '</td><td>' + content[i].value +
+        '</td><td><button class="edit">Edit</button> ' +
+        '<button class="delete">Delete</button></td></tr>';
     tableContent += eachLine;
 }
 table.children[0].innerHTML = tableContent;
 fitWidth(table, head); // 动态生成后再适配宽度
 
 const sidebar = document.getElementById('left-sidebar');
-// 设置左侧栏最大高度
-sidebar.style.height = 'auto'; // Before you ask for the height of the document inside the element you should set the height of the element to "auto".
+sidebar.style.height = 'auto'; // Before you ask for the height of the document          
+                               // inside the element you should set the height
+                               // of the element to "auto".
 sidebar.style.maxHeight = (sidebar.scrollHeight + 2) + 'px'; // 2px边框
 
 document.addEventListener('scroll', scrollPage);
+
+window.addEventListener('resize', resizeWindow);
+
+sidebar.addEventListener('click', toggleLevel2);
+
+const editDialog = document.getElementById('edit-dialog');
+const deleteDialog = document.getElementById('delete-dialog');
+const layer = document.getElementById('layer');
+const editName = document.getElementById('edit-name');
+const editContent = document.getElementById('edit-content');
+const editValue = document.getElementById('edit-value');
+let editRow;
+let deleteRow;
+document.addEventListener('click', controllTable);
+
 /**
- * 
+ * @function 右侧滑动时的事件处理程序
  * @param {document的scroll事件对象} e 
  */
 function scrollPage(e) {
@@ -44,22 +60,25 @@ function scrollPage(e) {
     }
 }
 
-window.addEventListener('resize', resizeWindow);
 /**
- * 
+ * @function 用于控制窗口大小改变时的事件处理程序，主要用来重新设置左侧栏高度及更新悬浮表头的位置
  * @param {window的resize事件对象} e 
  */
 function resizeWindow(e) {
     // 实时设置左侧栏高度
     sidebar.style.height = (window.innerHeight - sidebar.getBoundingClientRect().top) + 'px';
+
+    // 更新悬浮表头的位置
+    let tableRect = table.getBoundingClientRect();
+    head.style.left = tableRect.left + 'px';
 }
 
-sidebar.addEventListener('click', toggleLevel2);
 /**
- * 
+ * @function 用于控制切换二级表单显示的事件处理程序
  * @param {侧边栏的click事件对象} e 
  */
 function toggleLevel2(e) {
+    e.stopPropagation();
     let level1Elements = e.currentTarget.getElementsByClassName('sidebar-item');
     // 设置是否显示子菜单
     if (e.target.className === 'sidebar-item') {
@@ -87,14 +106,57 @@ function toggleLevel2(e) {
         }
     }
 
-    sidebar.style.height = 'auto'; // Before you ask for the height of the document inside the element you should set the height of the element to "auto".
+    sidebar.style.height = 'auto'; // Before you ask for the height of the document          
+                                   // inside the element you should set the height
+                                   // of the element to "auto".
     sidebar.style.maxHeight = (sidebar.scrollHeight + 2) + 'px'; // 2px边框
     sidebar.style.height = (window.innerHeight - sidebar.getBoundingClientRect().top) + 'px'; // 这里其实不设置也可以，设置回去我安心一点...
 }
 
 /**
+ * @function 用于控制点击编辑和删除按钮，及操作浮层的事件处理程序
+ * @param {document的click事件对象} e 
+ */
+function controllTable(e) {
+    switch (e.target.className) {
+        case 'edit':
+            editRow = e.target.parentNode.parentNode;
+            editName.value = editRow.children[0].innerHTML;
+            editContent.value = editRow.children[1].innerHTML;
+            editValue.value = editRow.children[2].innerHTML;
+            toggleDialog(editDialog);
+            break;
+        case 'delete':
+            deleteRow = e.target.parentNode.parentNode;
+            toggleDialog(deleteDialog);
+            break;
+        case 'confirm-edit':
+            e.preventDefault();
+            editRow.children[0].innerHTML = editName.value;
+            editRow.children[1].innerHTML = editContent.value;
+            editRow.children[2].innerHTML = editValue.value;
+            fitWidth(table, head);
+            toggleDialog(editDialog);
+            break;
+        case 'confirm-delete':
+            deleteRow.outerHTML = '';
+            toggleDialog(deleteDialog);
+            break;
+        case 'cancel':
+            e.preventDefault();
+            closeDialog();
+            break;
+        case 'layer':
+            closeDialog();
+            break;
+        default:
+            break;
+    }
+}
+
+/**
  * 
- * 
+ * @function 用于适配悬浮表头的宽度
  * @param {主表的引用} table 
  * @param {悬浮表头表的引用} head 
  */
@@ -103,4 +165,30 @@ function fitWidth(table, head) {
     head.querySelector('#head-2').style.width = window.getComputedStyle(table.querySelector('#main-2')).width;
     head.querySelector('#head-3').style.width = window.getComputedStyle(table.querySelector('#main-3')).width;
     head.querySelector('#head-4').style.width = window.getComputedStyle(table.querySelector('#main-4')).width;
+}
+
+/**
+ * @function 用于切换浮层的显示状态
+ * @param {需要切换显示的浮层} dialog 
+ */
+function toggleDialog(dialog) {
+    if (window.getComputedStyle(dialog).display === 'none') {
+        dialog.style.display = 'block';
+        layer.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+    } else {
+        dialog.style.display = 'none';
+        layer.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    }
+}
+
+/**
+ * @function 关闭所有浮层显示
+ */
+function closeDialog() {
+    editDialog.style.display = 'none';
+    deleteDialog.style.display = 'none';
+    layer.style.display = 'none';
+    document.body.style.overflow = 'auto';
 }
