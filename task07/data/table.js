4238284b01827e89fd94acb78db09a5b2a63e8e2
StudentWan <benyuwan@gmail.com>
2017-07-13 15:34:51 +0800

    add: task09 readme

--- task07/data/table.js
+++ task07/data/table.js
@@ -1,22 +1,112 @@
-let content = { '0': { name: 'Hello', content: 'Hello World!', value: 0 },
-  '1': { name: 'Hello', content: 'Hello World!', value: 1111 },
-  '2': { name: 'Hello', content: 'Hello World!', value: 2222 },
-  '3': { name: 'Hello', content: 'Hello World!', value: 3333 },
-  '4': { name: 'Hello', content: 'Hello World!', value: 4444 },
-  '5': { name: 'Hello', content: 'Hello World!', value: 5555 },
-  '6': { name: 'Hello', content: 'Hello World!', value: 6666 },
-  '7': { name: 'Hello', content: 'Hello World!', value: 7777 },
-  '8': { name: 'Hello', content: 'Hello World!', value: 8888 },
-  '9': { name: 'Hello', content: 'Hello World!', value: 9999 },
-  '10': { name: 'Hello', content: 'Hello World!', value: 11110 },
-  '11': { name: 'Hello', content: 'Hello World!', value: 12221 },
-  '12': { name: 'Hello', content: 'Hello World!', value: 13332 },
-  '13': { name: 'Hello', content: 'Hello World!', value: 14443 },
-  '14': { name: 'Hello', content: 'Hello World!', value: 15554 },
-  '15': { name: 'Hello', content: 'Hello World!', value: 16665 },
-  '16': { name: 'Hello', content: 'Hello World!', value: 17776 },
-  '17': { name: 'Hello', content: 'Hello World!', value: 18887 },
-  '18': { name: 'Hello', content: 'Hello World!', value: 19998 },
-  '19': { name: 'Hello', content: 'Hello World!', value: 21109 },
-  '20': { name: 'Hello', content: 'Hello World!', value: 22220 },
-  '21': { name: 'Hello', content: 'Hello World!', value: 23331 } }
+let content = {
+  "0": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 0
+  },
+  "1": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 1111
+  },
+  "2": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 2222
+  },
+  "3": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 3333
+  },
+  "4": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 4444
+  },
+  "5": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 5555
+  },
+  "6": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 6666
+  },
+  "7": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 7777
+  },
+  "8": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 8888
+  },
+  "9": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 9999
+  },
+  "10": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 11110
+  },
+  "11": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 12221
+  },
+  "12": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 13332
+  },
+  "13": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 14443
+  },
+  "14": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 15554
+  },
+  "15": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 16665
+  },
+  "16": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 17776
+  },
+  "17": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 18887
+  },
+  "18": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 19998
+  },
+  "19": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 21109
+  },
+  "20": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 22220
+  },
+  "21": {
+    "name": "Hello",
+    "content": "Hello World!",
+    "value": 23331
+  }
+}
