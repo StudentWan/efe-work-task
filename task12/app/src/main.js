7e7a5dcdd4f39b1f6f2ef324f734b13e39b96a8c
StudentWan <benyuwan@gmail.com>
2017-07-20 11:24:30 +0800

    change: change structure of task12

--- task12/app/src/main.js
+++
@@ -1,8 +0,0 @@
-import './static/style.less'
-
-let test = document.getElementById('root')
-test.innerHTML = 'Hello World'
-
-console.log(Hammer)
-
-console.log($('#root'))
--- task12/app/webpack.config.js
+++ task12/app/webpack.config.js
@@ -17,12 +17,22 @@ module.exports = {
             {
                 test: /\.less$/,
                 use: [{
-                    loader: "style-loader" 
+                    loader: "style-loader"
                 }, {
-                    loader: "css-loader" 
+                    loader: "css-loader",
+                    options: {
+                        modules: true,
+                        localIdentName: '[path][name]__[local]--[hash:base64:5]'
+                    }
                 }, {
-                    loader: "less-loader" 
+                    loader: "less-loader"
                 }]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     },
