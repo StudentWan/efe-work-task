2719be76f57fb0fedc289f96da7e4aeb8e6e03ba
StudentWan <benyuwan@gmail.com>
2017-08-07 21:14:04 +0800

    update: update task14 structure

--- task14/src/main.js
+++ task14/src/main.js
@@ -3,6 +3,7 @@ import san from 'san'
 import App from './components/App.san'
 import Onething from './components/Onething.san'
 import All from './components/All.san'
+import Edit from './components/Edit.san'
 
 import {router} from 'san-router'
 
@@ -13,11 +14,19 @@ import './static/scss/layout.scss'
 const app = new App()
 app.attach(document.body)
 
-
 // router
-router.add({rule: '/', Component: Onething, target: '#main'});
-router.add({rule: '/all', Component: All, target: '#main'})
+router.add({rule: '/', handler(e) {
+    app.data.set('path', 0)
+}})
+router.add({rule: '/all', handler(e) {
+    app.data.set('path', 1)
+}})
+router.add({rule: '/edit/:id', handler(e) {
+    app.data.set('path', 2)
+}})
 router.start()
+
+
 
 
 
